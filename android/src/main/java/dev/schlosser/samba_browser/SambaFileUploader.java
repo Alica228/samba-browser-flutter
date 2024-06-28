6551c0f825fbef335b54a37bdd7ba8c1802832b0
Alica228 <yura25051999@yandex.ru>
2024-06-28 09:19:32 +0200

    +

--- android/src/main/java/dev/schlosser/samba_browser/SambaFileUploader.java
+++ android/src/main/java/dev/schlosser/samba_browser/SambaFileUploader.java
@@ -22,7 +22,7 @@ import jcifs.smb.SmbFileOutputStream;
 
 public class SambaFileUploader {
 
-    private static final int FILE_CACHE_SIZE = 128 * 1024;
+    private static final int FILE_CACHE_SIZE = 128 * 1024 * 1024;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     static void uploadFile(MethodCall call, MethodChannel.Result result)  {
